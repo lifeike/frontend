a4801ee3e171eb7dcbb5fc2a7d57b4431ef145cc
lifeike <lifeike@gmail.com>
2022-12-05 21:32:00 -0700

    ad

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,7 +1,7 @@
 import UserList from "./UserList"
 import AddUser from "./AddUser"
 import UpdateUser from "./UpdateUser"
-import HomeLay from "../../components/Layout/Home"
+import HomeLay from "../../components/Layout/HomeLayout"
 
 function Users() {
   return (
