7dd56a0d5f0742e09ec674988c5894f61580119b
lifeike <lifeike@gmail.com>
2022-12-05 21:34:19 -0700

    ad

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,47 +1,21 @@
 import React, { useState, useRef, useEffect, memo } from "react"
-import { Routes, redirect, Route, Link, useNavigate, useLocation, Navigate, Outlet } from "react-router-dom"
-
-import { useForm } from "react-hook-form"
-import { signIn } from "../../api/auth"
-
-import Box from "@mui/material/Box"
-import Switch from "@mui/material/Switch"
-import Paper from "@mui/material/Paper"
-import Collapse from "@mui/material/Collapse"
-import FormControlLabel from "@mui/material/FormControlLabel"
+import SignInLayout from "@/components/Layout/SignInLayout"
 
 const SignIn = (props) => {
-  const [checked, setChecked] = React.useState(true)
-  useEffect(() => {
-    //shrink image on first page load
-    setChecked((prev) => !prev)
-  }, [])
-
   return (
-    <Box sx={{ height: 300 }}>
-      <Box
-        sx={{
-          "& > :not(style)": {
-            // display: "flex",
-            justifyContent: "space-around",
-            // height: 120,
-            width: "full",
-          },
-        }}>
-        <div className="relative">
-          <Box>
-            <Collapse orientation="horizontal" timeout={3000} in={checked} collapsedSize={"33%"}>
-              <Paper elevation={4}>
-                <Box component="div" sx={{ width: "100vw", height: "100vh" }}>
-                  <img src={require("@/assets/img/car.jpg")} alt=""></img>
-                </Box>
-              </Paper>
-            </Collapse>
-          </Box>
-          <div className="absolute top-5 right-5 -z-40 border border-black w-2/3 grid place-items-center"> hello world</div>
-        </div>
-      </Box>
-    </Box>
+    <SignInLayout>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+      <div>form </div>
+    </SignInLayout>
   )
 }
 
