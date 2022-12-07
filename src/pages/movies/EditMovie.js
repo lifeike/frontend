63f7b4c9790382788f8dcfc6963391146f39ad80
lifeike <lifeike@gmail.com>
2022-12-07 01:35:22 -0700

    ad

--- src/pages/movies/EditMovie.js
+++ src/pages/movies/EditMovie.js
@@ -1,11 +1,16 @@
 import React, { useState, useRef, useEffect, memo } from "react"
+import { useParams, useSearchParams } from "react-router-dom"
+
 import HomeLayout from "@/components/Layout/HomeLayout"
 
 const EditMovie = (props) => {
+  const { id } = useParams()
+
   return (
     <HomeLayout>
       <div>
         <h2>EditMovie</h2>
+        <div>{id}</div>
       </div>
     </HomeLayout>
   )
