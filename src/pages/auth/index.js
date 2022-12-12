99d1034616397285615ce8417800241dcfe5c924
lifeike <lifeike@gmail.com>
2022-12-11 18:26:34 -0700

    ad

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -2,10 +2,9 @@ import * as React from "react"
 import { Routes, Route, Link, useNavigate, useLocation, Navigate, Outlet } from "react-router-dom"
 
 export function RequireAuth({ children }) {
-  const access_token = JSON.parse(localStorage.getItem("session")).access_token
   const location = useLocation()
 
-  if (!access_token) {
+  if (!JSON.parse(localStorage.getItem("session"))?.access_token) {
     return <Navigate to="/" state={{ from: location }} replace />
   }
   return children
