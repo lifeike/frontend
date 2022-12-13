e95d7523dd2ce7496f99e04bc5b167f7160c4728
lifeike <lifeike@gmail.com>
2022-12-13 00:11:08 -0700

    ad

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,11 +1,24 @@
 export const getSession = async () => {
-  localStorage.getItem("session")
+  let sessionJson = localStorage.getItem("session")
+  if (sessionJson) {
+    try {
+      return JSON.parse(sessionJson)
+    } catch (err) {
+      return
+    }
+  }
+  return
 }
 
 export const setSession = async (session) => {
-  localStorage.setItem("session", session)
+  localStorage.setItem("session", JSON.stringify(session))
 }
 
 export const clearSession = async () => {
-  localStorage.setItem("")
+  localStorage.removeItem("session")
+}
+
+export const isAuthenticated = () => {
+  const session = getSession()
+  return session !== undefined && session.access_token !== undefined && session.refresh_token !== undefined
 }
