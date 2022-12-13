7540bf854065f88d28cfb4731923c9d0d99453a3
lifeike <lifeike@gmail.com>
2022-12-13 00:56:13 -0700

    ad

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import Movies from "./pages/movies"
-import EditMovie from "./pages/movies/EditMovie"
-import SignIn from "./pages/auth/SignIn"
-import Dashboard from "./pages/Dashboard"
-
-import { Routes, Route, Outlet, Link } from "react-router-dom"
 import Loading from "./components/Loading"
+import RouteTable from "./routes"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-
-import * as Auth from "./pages/auth"
 
 function App() {
   return (
     <>
       <ToastContainer />
       <Loading />
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route
-          path="/movies"
-          element={
-            <Auth.RequireAuth>
-              <Movies />
-            </Auth.RequireAuth>
-          }
-        />
-        <Route
-          path="/edit-movie/:id"
-          element={
-            <Auth.RequireAuth>
-              <EditMovie />
-            </Auth.RequireAuth>
-          }
-        />
-      </Routes>
+      <RouteTable />
     </>
   )
 }
---
+++ src/routes.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef, useEffect, memo } from "react"
+import { Routes, Route, Link, useNavigate, useLocation, Navigate, Outlet } from "react-router-dom"
+import * as session from "@/utils/session"
+
+import Movies from "./pages/movies"
+import EditMovie from "./pages/movies/EditMovie"
+import SignIn from "./pages/auth/SignIn"
+import Dashboard from "./pages/Dashboard"
+
+function RequireAuth({ children }) {
+  const location = useLocation()
+  if (!session.isAuthenticated()) {
+    return <Navigate to="/" state={{ from: location }} replace />
+  }
+  return children
+}
+
+const RouteTable = (props) => {
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/movies"
+        element={
+          <RequireAuth>
+            <Movies />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/edit-movie/:id"
+        element={
+          <RequireAuth>
+            <EditMovie />
+          </RequireAuth>
+        }
+      />
+    </Routes>
+  )
+}
+
+export default RouteTable
