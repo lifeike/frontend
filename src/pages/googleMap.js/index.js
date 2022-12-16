447d35754e0f744aba493b5f42da34877dd82c93
lifeike <lifeike@gmail.com>
2022-12-16 00:22:06 -0700

    ad

--- src/pages/googleMap.js/index.js
+++ src/pages/googleMap.js/index.js
@@ -1,11 +1,51 @@
 import React, { useState, useRef, useEffect, memo } from "react"
+import GooglePlacesAutocomplete from "react-google-places-autocomplete"
+import { geocodeByPlaceId } from "react-google-places-autocomplete"
+
 import HomeLayout from "@/components/layout/HomeLayout"
 
 const GoogleMapAutoComplete = (props) => {
+  const [value, setValue] = useState(null)
+
+  useEffect(() => {
+    console.log(value)
+    if (value) {
+      geocodeByPlaceId(value.value.place_id)
+        .then((results) => console.log(results))
+        .catch((error) => console.error(error))
+    }
+  }, [value])
+
   return (
     <HomeLayout>
       <p>API Key: AIzaSyB--wzBTOtKZtlXSY9QmLF4Ub5evrU6E8s</p>
       <h2>GoogleMapAutoComplete</h2>
+      <div>
+        <GooglePlacesAutocomplete
+          apiKey="AIzaSyB--wzBTOtKZtlXSY9QmLF4Ub5evrU6E8s"
+          selectProps={{
+            value,
+            onChange: setValue,
+          }}
+          apiOptions={{ language: "ca", region: "ca" }}
+          // selectProps={{
+          //   styles: {
+          //     input: (provided) => ({
+          //       ...provided,
+          //       color: "blue",
+          //     }),
+          //     option: (provided) => ({
+          //       ...provided,
+          //       color: "blue",
+          //     }),
+          //     singleValue: (provided) => ({
+          //       ...provided,
+          //       color: "blue",
+          //     }),
+          //   },
+          // }}
+        />
+      </div>
     </HomeLayout>
   )
 }
