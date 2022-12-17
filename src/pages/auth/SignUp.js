f2b4813b2d499acd7014b526dba12d8ef377fd68
lifeike <lifeike@gmail.com>
2022-12-17 01:47:23 -0700

    ad

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,12 +1,24 @@
 import React, { useState, useRef, useEffect, memo } from "react"
 import SignInLayout from "@/components/layout/SignInLayout"
 
-const SignUp = (props) => {
+import { useForm } from "react-hook-form"
+
+export default function SignUp() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data) => console.log(data)
+
   return (
     <SignInLayout>
-      <h2>SignUp</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>first_name</label>
+        <input className="block border my-2" {...register("firstName", { required: true, maxLength: 20 })} />
+        <label>last_name</label>
+        <input className="block border my-2" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        <label>email</label>
+        <input className="block border my-2" type="number" {...register("age", { min: 18, max: 99 })} />
+        <label>password</label>
+        <input className="block border my-2" type="submit" />
+      </form>
     </SignInLayout>
   )
 }
-
-export default SignUp
