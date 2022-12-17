0bb1c81196dd66d5b4dc41dc4e8f6fcf4f14a6f6
lifeike <lifeike@gmail.com>
2022-12-17 02:24:25 -0700

    ad

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -5,24 +5,37 @@ import { useForm } from "react-hook-form"
 import Button from "@mui/material/Button"
 import DeleteIcon from "@mui/icons-material/Delete"
 import SendIcon from "@mui/icons-material/Send"
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye"
 
 export default function SignUp() {
-  const { register, handleSubmit } = useForm()
+  const [showPassword, setShowPassword] = useState(true)
+  const { register, handleSubmit, resetField } = useForm()
   const onSubmit = (data) => console.log(data)
+  const handleClear = () => {
+    resetField("firstName")
+    resetField("lastName")
+    resetField("email")
+    resetField("password")
+  }
 
   return (
     <SignInLayout>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>first_name</label>
-        <input className="block border my-2" {...register("firstName", { required: true })} />
+        <input className="block border my-2 outline-none" {...register("firstName", { required: true })} />
         <label>last_name</label>
-        <input className="block border my-2" {...register("lastName", { required: true })} />
+        <input className="block border my-2 outline-none" {...register("lastName", { required: true })} />
         <label>email</label>
-        <input className="block border my-2" type="email" {...register("email", { required: true })} />
+        <input className="block border my-2 outline-none" type="email" {...register("email", { required: true })} />
         <label>password</label>
-        <input className="block border my-2" type="password" {...register("password", { required: true })} />
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
-          Delete
+        <div className="relative">
+          <input className="block border my-2 outline-none" type={`${showPassword ? "password" : ""}`} {...register("password", { required: true })} />
+          <div onClick={() => setShowPassword(!showPassword)} className="absolute right-0 top-0">
+            <RemoveRedEyeIcon />
+          </div>
+        </div>
+        <Button onClick={handleClear} variant="outlined" startIcon={<DeleteIcon />}>
+          Clear
         </Button>
         &nbsp; &nbsp; &nbsp;
         <Button type="submit" variant="contained" endIcon={<SendIcon />}>
