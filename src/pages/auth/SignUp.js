5bcbf2f6d36c7b8bd949b16d8ce6750f882a2d59
lifeike <lifeike@gmail.com>
2022-12-17 01:51:09 -0700

    ad

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useEffect, memo } from "react"
 import SignInLayout from "@/components/layout/SignInLayout"
-
 import { useForm } from "react-hook-form"
+
+import Button from "@mui/material/Button"
+import DeleteIcon from "@mui/icons-material/Delete"
+import SendIcon from "@mui/icons-material/Send"
 
 export default function SignUp() {
   const { register, handleSubmit } = useForm()
@@ -15,9 +18,17 @@ export default function SignUp() {
         <label>last_name</label>
         <input className="block border my-2" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
         <label>email</label>
-        <input className="block border my-2" type="number" {...register("age", { min: 18, max: 99 })} />
+        <input className="block border my-2" type="number" {...register("email")} />
         <label>password</label>
-        <input className="block border my-2" type="submit" />
+        <input className="block border my-2" type="number" {...register("password")} />
+        {/* <input className="block border my-2" type="submit" /> */}
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        &nbsp; &nbsp; &nbsp;
+        <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
       </form>
     </SignInLayout>
   )
