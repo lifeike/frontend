f282b29454c55af2ac03af11fa7c322589f157e6
lifeike <lifeike@gmail.com>
2022-12-19 06:14:21 -0700

    ad

---
+++ src/store/actionCreators/user.js
@@ -0,0 +1,13 @@
+import * as USER_API from "@/api/user"
+import * as session from "@/utils/session"
+import to from "await-to-js"
+
+export function searchUser(data) {
+  return async (dispatch, getState) => {
+    let [err, response] = await to(USER_API.searchUser(data))
+    if (err) {
+      return Promise.reject(err.message)
+    }
+    return response
+  }
+}
