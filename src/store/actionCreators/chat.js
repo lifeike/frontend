0b4fa49c4c68610da17f60f6c916baadef548637
lifeike <lifeike@gmail.com>
2022-12-19 18:51:53 -0700

    ad

---
+++ src/store/actionCreators/chat.js
@@ -0,0 +1,12 @@
+import * as CHAT_API from "@/api/chat"
+import to from "await-to-js"
+
+export function createChat(data) {
+  return async (dispatch, getState) => {
+    let [err, response] = await to(CHAT_API.createChat(data))
+    if (err) {
+      return Promise.reject(err.message)
+    }
+    dispatch({ type: "loading/turnOff" })
+  }
+}
