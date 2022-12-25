ba38278885345b5a0a4381dcdd9fd2ce9e07134a
lifeike <lifeike@gmail.com>
2022-12-25 02:46:43 -0700

    ad

--- src/pages/chat-room/index.js
+++ src/pages/chat-room/index.js
@@ -1,36 +1,12 @@
 import React, { useState, useRef, useEffect, memo } from "react"
-import { connect } from "react-redux"
 import HomeLayout from "@/components/layout/HomeLayout"
-import FriendsList from "./FriendsList"
-import FriendProfile from "./FriendProfile"
-import ChatArea from "./ChatArea"
-import io from "socket.io-client"
 
 const ChatRoom = (props) => {
-  const [socket, setSocket] = useState()
-  const [connected, setConnected] = useState(false)
-
-  const loadSocket = async () => {
-    setSocket(io("http://localhost:8080"))
-    await socket.emit("setup", props.user)
-    await socket.on("connected", () => setConnected(true))
-  }
-
-  useEffect(() => {
-    loadSocket()
-  }, [])
-
   return (
     <HomeLayout>
-      <div className="grid grid-cols-4 space-x-2 ">
-        <div className="col-span-1  chat-columns">{socket && <FriendsList socket={socket} />}</div>
-        <div className="col-span-2  chat-columns">{socket && <ChatArea socket={socket} />}</div>
-        <div className="col-span-1  chat-columns">
-          <FriendProfile />
-        </div>
-      </div>
+      <h2>Chat</h2>
     </HomeLayout>
   )
 }
 
-export default connect((state) => state.user, null)(ChatRoom)
+export default ChatRoom
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux"
 import user from "./user"
-import chat from "./chat"
 import movie from "./movie"
 import loading from "./loading"
 
-export default combineReducers({ user, loading, movie, chat })
+export default combineReducers({ user, loading, movie })
