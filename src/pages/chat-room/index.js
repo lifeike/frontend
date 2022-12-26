b5b780e0581530299f0a101d2e2f4788c1efc17a
lifeike <lifeike@gmail.com>
2022-12-26 05:33:15 -0700

    ad

--- src/pages/chat-room/index.js
+++ src/pages/chat-room/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux"
 import HomeLayout from "@/components/layout/HomeLayout"
 
 const ChatRoom = (props) => {
+  const [roomId, setroomId] = useState()
   let ws = null
   useEffect(() => {
     ws = new WebSocket("ws://localhost:8080")
@@ -17,7 +18,12 @@ const ChatRoom = (props) => {
   }, [])
 
   const sendMessage = async () => {
-    ws.send("你好阿")
+    ws.send({ meta: "", roomId, message: "test message." })
+  }
+
+  const joinRoom = async (roomId) => {
+    setroomId(roomId)
+    ws.send({ meta: "join", roomId, message: "I have joined" })
   }
 
   return (
@@ -27,10 +33,18 @@ const ChatRoom = (props) => {
       <h2>example: https://github.com/websockets/ws/blob/master/examples/server-stats/index.js</h2>
       <div className="w-full border border-black grid grid-cols-4">
         <div className="col-span-1 border border-black">
-          <div className="p-2 border m-2 rounded-sm">room 1</div>
-          <div className="p-2 border m-2 rounded-sm">room 2</div>
-          <div className="p-2 border m-2 rounded-sm">room 3</div>
-          <div className="p-2 border m-2 rounded-sm">room 4</div>
+          <div className="p-2 border m-2 rounded-sm" onClick={() => joinRoom(1)}>
+            room 1
+          </div>
+          <div className="p-2 border m-2 rounded-sm" onClick={() => joinRoom(2)}>
+            room 2
+          </div>
+          <div className="p-2 border m-2 rounded-sm" onClick={() => joinRoom(3)}>
+            room 3
+          </div>
+          <div className="p-2 border m-2 rounded-sm" onClick={() => joinRoom(4)}>
+            room 4
+          </div>
         </div>
         <div className="col-span-3 border border-black">
           <div> chat area</div>
