a44dc7c6e139df1dabdcc2bcfd62729f5c621e1c
lifeike <lifeike@gmail.com>
2022-12-27 07:53:02 -0700

    ad

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,5 +1,5 @@
 //backend api
-export const baseURL = process.env.NODE_ENV == "development" ? "http://localhost:3000/api/localhost" : "https://main.d2opyrywnuqr8v.amplifyapp.com/"
+export const baseURL = process.env.NODE_ENV == "development" ? "http://localhost:3000/api/localhost" : "http://localhost:8080"
 
 //configure setupProxy.js
 //other api 1
