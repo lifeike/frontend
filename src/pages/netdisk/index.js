8f254e7576a6cf7e3a13714f1b81dcd465e80b3b
lifeike <lifeike@gmail.com>
2022-12-31 03:04:05 -0700

    ad

---
+++ src/pages/netdisk/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useRef, useEffect, memo } from "react"
+
+const NetDisk = (props) => {
+  return (
+    <div>
+      <h2>NetDisk</h2>
+      <h1> need this to implement: https://www.modb.pro/db/473847</h1>
+    </div>
+  )
+}
+
+export default NetDisk
