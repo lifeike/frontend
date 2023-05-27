550942673602b028331e78c4a66e7fa8537e91a2
lifeike <lifeike@gmail.com>
2023-05-27 16:33:11 -0600

    fix

--- src/pages/devlift/index.js
+++ src/pages/devlift/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useRef, useEffect, memo } from "react"
 import HomeLay from "@/components/layout/HomeLayout"
 import { doc, getFirestore } from "firebase/firestore"
-import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from "reactfire"
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirestoreCollection, useFirebaseApp } from "reactfire"
 
 const DevLift = (props) => {
-  // easily access the Firestore library
+  // set up query
   const burritoRef = doc(useFirestore(), "movies", "4eO9e19BjNPaUKeSMeY3")
-
-  // subscribe to a document for realtime updates. just one line!
   const { status, data } = useFirestoreDocData(burritoRef)
   console.log(data)
 
