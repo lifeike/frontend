9a445ccfd8c4ba0713370b216744ec65ab86b485
lifeike <lifeike@gmail.com>
2023-05-27 17:09:57 -0600

    fix

--- src/pages/devlift/index.js
+++ src/pages/devlift/index.js
@@ -2,6 +2,24 @@ import React, { useState, useRef, useEffect, memo } from "react"
 import HomeLay from "@/components/layout/HomeLayout"
 import { doc, getFirestore, collection, getDoc } from "firebase/firestore"
 import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirestoreCollection, useFirebaseApp } from "reactfire"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+]
 
 const DevLift = (props) => {
   // set up query
@@ -10,7 +28,8 @@ const DevLift = (props) => {
   // console.log(data)
   const colRef = collection(useFirestore(), "movies")
   const { status, data } = useFirestoreCollection(colRef)
-  console.log(data.docs)
+  console.log(data?.docs)
+  // console.log(data.docs)
 
   // easily check the loading status
   if (status === "loading") {
@@ -20,6 +39,32 @@ const DevLift = (props) => {
   return (
     <HomeLay>
       <h2>DevLift</h2>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </HomeLay>
   )
 }
