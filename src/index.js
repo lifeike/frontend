c6cb1e25c8327b32028d6fca667ce8b00f2a3f90
lifeike <lifeike@gmail.com>
2023-05-27 16:00:22 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import * as session from "@/utils/session"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import "./index.css"
+import { FirebaseAppProvider, useFirebaseApp } from "reactfire"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDz7vrq6HK226xWCedn0Bb55ezTbuFfv_E",
+  authDomain: "devlift-5405d.firebaseapp.com",
+  projectId: "devlift-5405d",
+  storageBucket: "devlift-5405d.appspot.com",
+  messagingSenderId: "636518484012",
+  appId: "1:636518484012:web:6c720bd67e404c5ce6a989",
+  measurementId: "G-5M917X6B4M",
+}
 
 //fullfill user redux profile on  page refresh
 if (session.getSession()) {
@@ -14,9 +25,11 @@ if (session.getSession()) {
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </FirebaseAppProvider>
 )
--- src/pages/devlift/index.js
+++ src/pages/devlift/index.js
@@ -1,7 +1,21 @@
 import React, { useState, useRef, useEffect, memo } from "react"
 import HomeLay from "@/components/layout/HomeLayout"
+import { doc, getFirestore } from "firebase/firestore"
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from "reactfire"
 
 const DevLift = (props) => {
+  // easily access the Firestore library
+  const burritoRef = doc(useFirestore(), "movies")
+
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData(burritoRef)
+  console.log(data)
+
+  // easily check the loading status
+  if (status === "loading") {
+    return <p>Fetching burrito flavor...</p>
+  }
+
   return (
     <HomeLay>
       <h2>DevLift</h2>
